5187f2bbc0a65811e366d4e508367dca6533aa74
GilbertChristensen <gilbertchristensen@gmail.com>
2020-10-07 09:27:17 +0200

    Fjernet header

--- src/financialcalculator/FinancialCalculator.java
+++ src/financialcalculator/FinancialCalculator.java
@@ -1,17 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package financialcalculator;
 
 import io.InputOutputTool;
 
-/**
- * 
- * @author alex
- */
 public class FinancialCalculator {
 
     /**
